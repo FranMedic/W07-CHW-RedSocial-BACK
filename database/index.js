a0b2b1c332b8b5d88b4f0e7211cde9aaf2625082
Franny Medico <m.francisca.medico@gmail.com>
2021-11-13 11:37:42 +0100

    added function to initiate db

---
+++ database/index.js
@@ -0,0 +1,34 @@
+const debug = require("debug")("redS:database");
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+
+const dBInitializer = (redSocialDB) =>
+  new Promise((resolve, reject) => {
+    mongoose.set("debug", true);
+    mongoose.set("toJSON", {
+      virtuals: true,
+
+      transform: (doc, ret) => {
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._id;
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret.__v;
+      },
+    });
+
+    mongoose.connect(redSocialDB, (error) => {
+      if (error) {
+        debug(chalk.red("Connection to DB failed  (╯°□°）╯︵ ┻━┻)"));
+        debug(chalk.red(error.message));
+        reject(error);
+      }
+
+      debug(chalk.magentaBright("Connection to DB succeful "));
+      resolve();
+    });
+    mongoose.connection.on("close", () => {
+      debug(chalk.blueBright("Desconectado de la DataBase"));
+    });
+  });
+
+module.exports = dBInitializer;
